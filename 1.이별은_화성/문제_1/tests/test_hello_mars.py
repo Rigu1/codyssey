9d81d5211940091f74037c56eb115aea45f66ed2
rigu <dlsrbtla@gmail.com>
2025-03-08 03:16:35 +0900

    test: Add string output test

---
+++ 1.이별은_화성/문제_1/tests/test_hello_mars.py
@@ -0,0 +1,12 @@
+# 파일명: test_hello_mars.py 
+# 설명: Hello Mars 출력의 테스트 코드
+# 작성자: Rigu1
+# 작성일: 2025-03-08
+
+def test_hello_mars(): 
+    if message == "Hello Mars":
+        print("🟢 PASS")
+    else :
+        print("🔴 FAIL")
+        
+test_hello_mars()

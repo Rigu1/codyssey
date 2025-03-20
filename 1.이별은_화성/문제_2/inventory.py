fc382e80a82eed82292db267c0bdba1350f8260d
rigu <dlsrbtla@gmail.com>
2025-03-20 14:35:10 +0900

    feat: add inventory data processing and storage features
    - ERROR_MESSAGES에 Exception 처리 추가하여 예외 처리 범위 확장
    - 인벤토리 데이터를 CSV에서 읽어오고 self.inventory_lines로 저장하는 기능 추가
    - get_sorted_items_desc 추가하여 인화성 기준 내림차순 정렬 기능 구현
    - get_items_above_threshold 추가하여 특정 임계값 이상의 항목을 필터링하는 기능 추가
    - save_dangerous_items 추가하여 필터링된 데이터를 CSV 파일로 저장하는 기능 추가
    - save_sorted_inventory_as_binary 추가하여 정렬된 데이터를 이진 파일로 저장하는 기능 추가
    - get_inventory_from_binary 추가하여 이진 파일에서 데이터를 불러오는 기능 추가

--- 1.이별은_화성/문제_2/inventory.py
+++ 1.이별은_화성/문제_2/inventory.py
@@ -8,6 +8,7 @@ class InventoryManager:
     ERROR_MESSAGES = {
         FileNotFoundError: 'File not found >> {}',
         PermissionError: 'Permission denied >> {}',
+        Exception: 'Unknown error >> {}',
     }
 
     STATIC_PATH = '/static'
@@ -16,23 +17,121 @@ class InventoryManager:
     TARGET_FILE_NAME = 'Mars_Base_Inventory_List.csv'
     TARGET_FILE_PATH = ROOT_PATH + STATIC_PATH + '/' + TARGET_FILE_NAME
 
-    def __init__(self):
-        self.inventory_lines = self._get_data_from_file()
+    OUTPUT_FILE_NAME = 'Mars_Base_Inventory_danger.csv'
+    OUTPUT_FILE_PATH = ROOT_PATH + STATIC_PATH + '/' + OUTPUT_FILE_NAME
 
-    def _get_data_from_file(self):
+    BINARY_FILE_NAME = 'Mars_Base_Inventory_List.bin'
+    BINARY_FILE_PATH = ROOT_PATH + STATIC_PATH + '/' + BINARY_FILE_NAME
+
+    DELIMITER = ','
+    REFERENCE_KEY = 'Flammability'
+    THRESHOLD = 0.7
+
+    def __init__(self):
+        self.inventory_lines = self._get_split_by_delimiter(
+            self._get_inventory_from_csv()
+        )
+        self.header = self.inventory_lines[0]
+        self.inventory_items = self.inventory_lines[1:]
+
+    def _get_inventory_from_csv(self):
         try:
             with open(self.TARGET_FILE_PATH, 'r', encoding='utf-8') as file:
                 return file.read().splitlines()
-        except (FileNotFoundError, PermissionError) as e:
-            error_message = self.ERROR_MESSAGES[type(e)].format(
-                self.TARGET_FILE_PATH
-            )
+        except Exception as e:
+            error_message = self.ERROR_MESSAGES.get(
+                type(e), self.ERROR_MESSAGES[Exception]
+            ).format(self.TARGET_FILE_PATH)
             raise type(e)(error_message) from e
+
+    def _get_split_by_delimiter(self, items):
+        return [line.split(self.DELIMITER) for line in items]
 
     def get_inventory_lines(self):
         return self.inventory_lines
+
+    def get_sorted_items_desc(self):
+        sort_key_index = self.header.index(self.REFERENCE_KEY)
+
+        sorted_inventory = sorted(
+            self.inventory_items,
+            key=lambda item: float(item[sort_key_index]),
+            reverse=True,
+        )
+
+        return [self.header] + sorted_inventory
+
+    def get_items_above_threshold(self):
+        reference_key_index = self.header.index(self.REFERENCE_KEY)
+
+        filtered_inventory = [
+            item
+            for item in self.inventory_items
+            if float(item[reference_key_index]) >= self.THRESHOLD
+        ]
+
+        return [self.header] + filtered_inventory
+
+    def save_dangerous_items(self):
+        dangerous_inventory = self.get_items_above_threshold()
+
+        try:
+            with open(self.OUTPUT_FILE_PATH, 'w', encoding='utf-8') as file:
+                for line in dangerous_inventory:
+                    file.write(self.DELIMITER.join(map(str, line)) + '\n')
+
+            print(f"Save dangerous items: {self.OUTPUT_FILE_PATH}")
+
+        except Exception as e:
+            error_message = self.ERROR_MESSAGES.get(
+                type(e), self.ERROR_MESSAGES[Exception]
+            ).format(self.OUTPUT_FILE_PATH)
+            raise type(e)(error_message) from e
+
+    def save_sorted_inventory_as_binary(self):
+        sorted_inventory = self.get_sorted_items_desc()
+
+        try:
+            with open(self.BINARY_FILE_PATH, 'wb') as file:
+                for line in sorted_inventory:
+                    line_bytes = (
+                        self.DELIMITER.join(map(str, line)) + '\n'
+                    ).encode('utf-8')
+                    file.write(line_bytes)
+
+            print(
+                f"Save sorted items as binary files: {self.BINARY_FILE_PATH}"
+            )
+
+        except Exception as e:
+            error_message = self.ERROR_MESSAGES.get(
+                type(e), self.ERROR_MESSAGES[Exception]
+            ).format(self.BINARY_FILE_PATH)
+            raise type(e)(error_message) from e
+
+    def get_inventory_from_binary(self):
+        try:
+            with open(self.BINARY_FILE_PATH, 'rb') as file:
+                binary_data = file.readlines()
+
+            inventory_list = [
+                line.decode('utf-8').strip() for line in binary_data
+            ]
+
+            return self._get_split_by_delimiter(inventory_list)
+
+        except Exception as e:
+            error_message = self.ERROR_MESSAGES.get(
+                type(e), self.ERROR_MESSAGES[Exception]
+            ).format(self.BINARY_FILE_PATH)
+            raise type(e)(error_message) from e
 
 
 if __name__ == '__main__':
     inventory_manager = InventoryManager()
     print(*inventory_manager.get_inventory_lines(), sep='\n')
+    print(*inventory_manager.get_sorted_items_desc(), sep='\n')
+    print(*inventory_manager.get_items_above_threshold(), sep='\n')
+    inventory_manager.save_dangerous_items()
+    inventory_manager.save_sorted_inventory_as_binary()
+    print(*inventory_manager.get_inventory_from_binary(), sep='\n')

7a20a264fef134b0c3d97a356304871080736120
rigu <dlsrbtla@gmail.com>
2025-03-24 20:23:47 +0900

    feat: Implementing the Dummy Sensor Class for dummy environmental data generation

---
+++ 1.이별은_화성/문제_3/mars_mission_computer.py
@@ -0,0 +1,35 @@
+# mars_mission_computer.py
+# 더미 센서 시스템
+# 2025-03-23
+
+import random
+
+
+class DummySensor:
+    ENV_VALUE_CONFIG = {
+        'mars_base_internal_temperature': (18, 30, 'int'),
+        'mars_base_external_temperature': (0, 21, 'int'),
+        'mars_base_internal_humidity': (50, 60, 'int'),
+        'mars_base_external_illuminance': (500, 715, 'int'),
+        'mars_base_internal_co2': (0.02, 0.1, 'float'),
+        'mars_base_internal_oxygen': (4, 7, 'int'),
+    }
+
+    def __init__(self):
+        self.env_values = {key: 0 for key in self.ENV_VALUE_CONFIG.keys()}
+
+    def set_env(self):
+        for key, (start, end, value_type) in self.ENV_VALUE_CONFIG.items():
+            if value_type == 'int':
+                self.env_values[key] = random.randint(start, end)
+            elif value_type == 'float':
+                self.env_values[key] = round(random.uniform(start, end), 2)
+
+    def get_env(self):
+        return self.env_values
+
+
+if __name__ == '__main__':
+    ds = DummySensor()
+    ds.set_env()
+    ds.get_env()
